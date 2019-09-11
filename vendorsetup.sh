0c28cc4bb705af61b737f32fb7a0dda7d5b0527d
Tim Schumacher <timschumi@gmx.de>
2019-09-11 11:00:22 +0200

    Add generic aosp targets to lunch
    Change-Id: I9e11e80df8db4ba314aa089e1d45b415f7d80b44

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,8 @@ for combo in $(curl -s https://raw.githubusercontent.com/KrakenProject/hudson/ma
 do
     add_lunch_combo $combo
 done
+
+add_lunch_combo aosp_arm-userdebug
+add_lunch_combo aosp_arm64-userdebug
+add_lunch_combo aosp_x86-userdebug
+add_lunch_combo aosp_x86_64-userdebug
