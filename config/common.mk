885f84e6eb2a8e31f689d1a4aa96c6e328dcc7db
Bruno Martins <bgcngm@gmail.com>
2020-12-01 21:22:46 +0000

    custom: Add Android Go specific configs
    Trebuchet is also available as a Go version, so modify the existing
    configs to allow it to be shipped in Android Go enabled devices.
    
    Change-Id: I29f203b3fa19bc955976ee6c3da4142a4ab325bf

--- config/common.mk
+++ config/common.mk
@@ -183,8 +183,7 @@ endif
 
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    TrebuchetQuickStep
+    SystemUI
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
