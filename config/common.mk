723c17763d3c2127d53109429eb4273750d49ce8
Alexandre Rangel <mamutal91@gmail.com>
2020-11-30 23:00:11 -0300

    vendor: disable Updater on common.mk

--- config/common.mk
+++ config/common.mk
@@ -113,8 +113,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard
+#    Updater
 
 # Themes
 PRODUCT_PACKAGES += \
