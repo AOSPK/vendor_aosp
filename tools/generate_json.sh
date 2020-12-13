491cd4173b7c60474e0d88d1026a1d066bc1675c
Alexandre Rangel <mamutal91@gmail.com>
2020-12-13 01:00:41 -0300

    build/tools: update json generator

--- tools/generate_json.sh
+++ tools/generate_json.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-zip=${1}
+zip_path=${1}
 version=${2}
 device=${3}
 type=${4}
 
-datetime=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.build.date.utc' $(dirname $zip)/system/build.prop)")
-zip_name=$(basename "$zip")
-md5=$(cat "$zip.md5sum" | cut -d' ' -f1)
-size=$(stat --printf="%s" "$zip")
+datetime=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.build.date.utc' $(dirname $zip_path)/system/build.prop)")
+zip=$(basename "$zip_path")
+md5=$(cat "$zip_path.md5sum" | cut -d' ' -f1)
+size=$(ls -lat $zip_path | cut -d ' ' -f 5)
 url="https://master.dl.sourceforge.net/project/aospk/$device/$zip_name"
 
 function generate_json() {
-  echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
-  echo
-  echo 'Information for official maintainers:'
-  echo 'After testing the build, publish to SourceForge'
-  echo 'Submit your build to the AOSPK/official_devices repository with the json content below.'
-  echo '$zip.json'
-  echo
   echo '    {'
   echo '      "datetime": "'$datetime'",'
-  echo '      "filename": "'$zip_name'",'
+  echo '      "filename": "'$zip'",'
   echo '      "id": "'$md5'",'
-  echo '      "romtype": "OFFICIAL",'
   echo '      "size": "'$size'",'
   echo '      "url": "'$url'",'
-  echo '      "version": "'$version'"'
+  echo '      "android": "'$version'"'
   echo '    }'
-  echo
 }
 
 if [ $type == "UNOFFICIAL" ];then
   echo
 else
   generate_json > $zip.json
+  echo
+  echo 'Information for official maintainers:'
+  echo 'After testing the build, publish to SourceForge'
+  echo 'Submit your build to the AOSPK/official_devices repository with the json content below.'
+  echo
   cat $zip.json
 fi
