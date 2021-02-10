0bf41d9186c31a87d7e069506cf6d89cace95772
Alexandre Rangel <mamutal91@gmail.com>
2021-02-10 06:59:37 -0300

    vendor: Switch to our version
    Change-Id: Ide62f67a32ddc2dca7b5009efcd377ca68f0b8c8

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,10 +6,10 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.lineage.version=$(CUSTOM_VERSION) \
+    ro.lineage.releasetype=$(CUSTOM__BUILD_TYPE) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
+    ro.modversion=$(APPS_VERSION) \
     ro.lineagelegal.url=https://lineageos.org/legal
 
 # LineageOS Platform Display Version
