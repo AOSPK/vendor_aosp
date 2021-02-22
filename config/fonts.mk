7dc453e1d70ce246542b1f7dfdf8e215b3d43a98
Arseniy Graur <argraur@aosip.dev>
2021-02-21 22:36:19 -0300

    vendor: Add fonts for customization and set Google Sans as default font.
    * These fonts were imported from the PixelExperience vendor. We respect authorship and co-authorship!
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I4e9c03ea57ea75e7e0c5a608981dbb21b125476e

---
+++ config/fonts.mk
@@ -0,0 +1,40 @@
+# Arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# Google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-BoldItalic.ttf \
+    GoogleSans-Bold.ttf \
+    GoogleSans-Italic.ttf \
+    GoogleSans-MediumItalic.ttf \
+    GoogleSans-Medium.ttf \
+    GoogleSans-Regular.ttf
+
+# Lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# Rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# Zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
