790a72d94aa5ce37ce499f7779956b87746d8d5f
Alexandre Rangel <mamutal91@gmail.com>
2021-04-04 03:06:22 -0300

    vendor/tools: New server to host builds
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I0ebb5e77e1255de00a2075ba9c75c5f3b3e29b9a

--- tools/generate_json.sh
+++ tools/generate_json.sh
@@ -4,13 +4,14 @@ zip_path=${1}
 version=${2}
 device=${3}
 type=${4}
+android=${5}
 
 datetime=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.build.date.utc' $(dirname $zip_path)/system/build.prop)")
 apps=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.custom.apps.version' $(dirname $zip_path)/system/build.prop)")
 zip=$(basename "$zip_path")
 md5=$(cat "$zip_path.md5sum" | cut -d' ' -f1)
 size=$(ls -lat $zip_path | cut -d ' ' -f 5)
-url="https://master.dl.sourceforge.net/project/aospk/$device/$zip"
+url="https://download.aospk.org/$android/$device/$apps/$zip"
 
 function generate_json() {
   echo '    {'
