53596fbf8721d0ebd861ee3f3b34c7a92694db45
Alexandre Rangel <mamutal91@gmail.com>
2021-04-07 03:53:15 -0300

    vendor: Update path to GApps
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: Iddb6ddfe846499bb6ba49c986c5f79adfe708132

--- config/version.mk
+++ config/version.mk
@@ -21,7 +21,7 @@ VANILLA_BUILD ?= false
 ifeq ($(VANILLA_BUILD), true)
     CUSTOM_APPS_VERSION := Vanilla
 else
-    $(call inherit-product-if-exists, vendor/google/gms/config.mk)
+    $(call inherit-product, vendor/gapps/common/common-vendor.mk)
     CUSTOM_APPS_VERSION := GApps
 endif
 
