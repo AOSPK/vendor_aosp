f0b9c95f073e885207c5a3fc7e982c048a670c78
Alexandre Rangel <mamutal91@gmail.com>
2021-04-06 23:26:48 -0300

    vendor: Update fp from redfin April patch
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I54cfac0e02ed00201600852f1141c3f14d90b91b

--- config/version.mk
+++ config/version.mk
@@ -1,6 +1,6 @@
 # Build fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210305.006 7119741 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ2A.210405.005 7181113 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
