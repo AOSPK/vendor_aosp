c50d0f32b8d7e0a71fd1f7176981cfdf3fc70a6c
Alexandre Rangel <mamutal91@gmail.com>
2021-05-01 18:24:03 -0300

    vendor: Move custom overlays to vendor
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I27591c1b58d3c95d51b59bfc2535168d30fe1aa6

--- config/common_full.mk
+++ config/common_full.mk
@@ -6,9 +6,3 @@ PRODUCT_SIZE := full
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    CustomLatoFont \
-    CustomRubikFont
---
+++ overlay/custom/accents/black/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := CustomBlackAccent
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := CustomBlackAccent
+LOCAL_OVERRIDES_PACKAGES := AccentColorBlackOverlay
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)
