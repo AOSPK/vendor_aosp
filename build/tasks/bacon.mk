ab5ef02782e2d7716d7a4d3102ffa14a6420c3e4
Julian Veit <claymore1298@gmail.com>
2021-05-05 13:22:03 -0300

    bacon: switch to sha256 checksum
    Change-Id: Ia546049bd3bbaeb7a30f97408c35f4a7b3e4a2f8
    Co-authored-by: Alexandre Rangel <mamutal91@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -18,12 +18,12 @@
 
 CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CUSTOM_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
-	$(hide) $(MD5) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(CUSTOM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CUSTOM_TARGET_PACKAGE).sha256sum
 	cat ./vendor/aosp/tools/ascii;
 	@echo -e ${CL_CYN}""${CL_CYN}
 	@echo "Build complete: $(CUSTOM_TARGET_PACKAGE)" >&2
