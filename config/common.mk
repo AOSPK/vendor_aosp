f6b73d3fb55ce6362e01cfec87e40937b1144001
Alexandre Rangel <mamutal91@gmail.com>
2021-06-07 17:44:29 -0300

    vendor: SetupWizard configuration
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I4673ebaba92f47c97786e570ebb4dfd9045d9ea2

--- config/common.mk
+++ config/common.mk
@@ -220,6 +220,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.theme_id=5 \
     ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
+# SetupWizard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.enable_assist_gesture_training=true \
+    setupwizard.theme=glif_v3_light \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=false
+
 # Google legal
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
