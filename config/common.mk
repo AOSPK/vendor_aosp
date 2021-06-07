044bb6644ec88317d8eb406d785a9971157dfc79
Alexandre Rangel <mamutal91@gmail.com>
2021-06-07 18:32:38 -0300

    vendor: Import new fonts
    Co-authored-by: Sourajit Karmakar <sourajit@live.com>
    Co-authored-by: Call190Now <call190now@outlook.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: paphonb <paphonb@gmail.com>
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I0ded2f04ddb169d927c8fc6467d84b7bc9933118

--- config/common.mk
+++ config/common.mk
@@ -260,5 +260,12 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     GoogleCameraGo
 
--include vendor/aosp/config/version.mk
--include vendor/aosp/config/fonts.mk
+# Fonts
+LOCAL_PATH := vendor/aosp/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Version
+$(call inherit-product, vendor/aosp/config/version.mk)
