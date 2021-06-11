5eb62e729b1c5a9ac3005c54e33373620589c629
Alexandre Rangel <mamutal91@gmail.com>
2021-06-11 19:32:39 -0300

    vendor: tools: json updates
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I7bd4d6f20f5ce0d6652f0d704e12be3bcc04f5e3

--- tools/generate_json.sh
+++ tools/generate_json.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 
-zip_path=${1}
+zipPath=${1}
 version=${2}
 device=${3}
 type=${4}
 android=${5}
 
-datetime=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.build.date.utc' $(dirname $zip_path)/system/build.prop)")
-apps=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.custom.apps.version' $(dirname $zip_path)/system/build.prop)")
-zip=$(basename "$zip_path")
-md5=$(cat "$zip_path.md5sum" | cut -d' ' -f1)
-size=$(ls -lat $zip_path | cut -d ' ' -f 5)
+dateTime=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.build.date.utc' $(dirname $zipPath)/system/build.prop)")
+apps=$(bash -c "cut -d'=' -f2 <<< $(grep 'ro.custom.apps.version' $(dirname $zipPath)/system/build.prop)")
+zip=$(basename "$zipPath")
+md5=$(cat "$zipPath.md5sum" | cut -d' ' -f1)
+size=$(ls -lat $zipPath | cut -d ' ' -f 5)
 url="https://master.dl.sourceforge.net/project/aospk/$android/$device/$apps/$zip"
+jsonPath=$(basename $zipPath .zip)
 
 function generate_json() {
-  echo '    {'
-  echo '      "datetime": "'$datetime'",'
-  echo '      "filename": "'$zip'",'
-  echo '      "id": "'$md5'",'
-  echo '      "size": "'$size'",'
-  echo '      "url": "'$url'",'
-  echo '      "romtype": "'$apps'",'
-  echo '      "android": "'$android'",'
-  echo '      "version": "'$version'"'
-  echo '    }'
+echo -e "\n  {
+    \"dateTime\": \"${dateTime}\",
+    \"filename\": \"${zip}\",
+    \"id\": \"${md5}\",
+    \"size\": \"${size}\",
+    \"url\": \"${url}\",
+    \"romtype\": \"${apps}\",
+    \"android\": \"${android}\",
+    \"version\": \"${version}\"
+  }"
 }
 
-if [ $type == "OFFICIAL" ];then
-  generate_json > $zip_path.json
-  echo
-  echo 'Information for official maintainers:'
-  echo 'After testing the build, publish to SourceForge'
-  echo 'Submit your build to the AOSPK/official_devices repository with the json content below.'
-  echo
-  cat $zip_path.json
+if [ $type = "OFFICIAL" ];then
+  generate_json > $jsonPath.json
+  echo -e "\nTESTEInformation for official maintainers:\nAfter testing the build, publish to SourceForge\nSubmit your build to the AOSPK/official_devices repository with the json content below."
+  cat $jsonPath.json
   echo
 fi
