a0b9724a6bd96ab6d6c4905a8c20ebf914047cef
Alexandre Rangel <mamutal91@gmail.com>
2021-06-14 01:32:53 -0300

    vendor: For devices with Notch
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: Ic9fa02017dfaf2cedd53492cf5f5afb3219f8c47

--- config/common.mk
+++ config/common.mk
@@ -198,6 +198,11 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
+# hasNotch
+ifneq (,$(filter begonia beryllium ginkgo jasmine_sprout joyeuse lavender mojito sweet,$(CUSTOM_DEVICE)))
+DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/hasnotch
+endif
+
 # Blurs
 ifneq (,$(filter msm8226 msm8610 msm8974 msm8992 msm8994 msm8909 msm8916 msm8937 msm8953 msm8996 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)))
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
