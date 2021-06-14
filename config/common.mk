18a63b1f6b2df9c9cc6019e33e85e93838a445b5
Alexandre Rangel <mamutal91@gmail.com>
2021-06-13 23:58:25 -0300

    vendor: Overlay for HasNotch
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: Idd62756c8dcc1940d4ab0f0c4185c4afae58042d

--- config/common.mk
+++ config/common.mk
@@ -197,10 +197,11 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 -include vendor/custom-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
--include vendor/aosp/config/partner_gms.mk
 
-# Pixel Features
-$(call inherit-product, vendor/google/pixel/config.mk)
+# hasNotch
+ifneq (,$(filter begonia beryllium ginkgo jasmine_sprout joyeuse lavender mojito sweet,$(CUSTOM_DEVICE)))
+DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/hasnotch
+endif
 
 # Blurs
 ifneq (,$(filter msm8226 msm8610 msm8974 msm8992 msm8994 msm8909 msm8916 msm8937 msm8953 msm8996 msm8998 sdm660,$(TARGET_BOARD_PLATFORM)))
