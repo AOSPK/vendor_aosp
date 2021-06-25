8996c4e329763b1b9c28419c21ea9dbd7b580450
Alexandre Rangel <mamutal91@gmail.com>
2021-06-25 00:24:55 -0300

    vendor: Rebrand our version
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I4bd36f3fc706e86a54bd1f10a91850426a67c574

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,34 +1,26 @@
-# CustomPlatform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.custom.display.version=$(CUSTOM_DISPLAY_VERSION)
-
-# CustomPlatform SDK Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.custom.build.version.plat.sdk=$(CUSTOM_PLATFORM_SDK_VERSION)
-
-# CustomPlatform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.custom.build.version.plat.rev=$(CUSTOM_PLATFORM_REV)
-
-# Kraken Verion
+# Custom props
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.custom.releasetype=$(CUSTOM_BUILD_TYPE) \
-    ro.custom.build.version=$(CUSTOM_ANDROID_VERSION) \
+    ro.custom.build.version=$(CUSTOM_PLATFORM_VERSION) \
     ro.modversion=$(CUSTOM_VERSION) \
-    ro.custom.display.version=$(CUSTOM_FULL_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE) \
+    ro.custom.display.version=$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_APPS_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE) \
     ro.custom.device=$(CUSTOM_DEVICE) \
-    ro.custom.build.date=$(CUSTOM_BUILD_DATE)
+    ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
+    ro.custom.display.version=$(CUSTOM_DISPLAY_VERSION) \
+    ro.custom.build.version.plat.sdk=$(CUSTOM_PLATFORM_SDK_VERSION) \
+    ro.custom.build.version.plat.rev=$(CUSTOM_PLATFORM_REV)
 
 # GMS
 ifeq ($(VANILLA_BUILD), true)
-    ADDITIONAL_BUILD_PROPERTIES += \
-        ro.custom.apps.version=vanilla
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.custom.apps.version=vanilla
 else
-    ADDITIONAL_BUILD_PROPERTIES += \
-        ro.custom.apps.version=gapps
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.custom.apps.version=gapps
 endif
 
+# Check maintainer
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 ifeq ($(CUSTOM_MAINTAINER),)
 $(error If you are OFFICIAL, you need to define the variable with your name in your device tree, for example: CUSTOM_MAINTAINER := MyName)
