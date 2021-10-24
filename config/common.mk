1a370e13ba0ffd4a9aee5efff3be15f80f0fc275
Alexandre Rangel <mamutal91@gmail.com>
2021-10-23 23:16:16 -0300

    Revert "vendor: Include NexusLauncher for dex preopt"
    This reverts commit 5d9205918ecd3e9678863dba8d06db7329d46b57.
    
    Change-Id: I2444bf66e0191e0b6a428bd5cd958d74a0044aac

--- config/common.mk
+++ config/common.mk
@@ -26,8 +26,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
-    Launcher3QuickStep \
-    NexusLauncherRelease
+    Launcher3QuickStep
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
