9a1d479662cd896153143ec14a46d6ed947ddb3a
Alexandre Rangel <mamutal91@gmail.com>
2022-03-30 05:59:22 -0300

    vendor: Re-add our bootanimation
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I9fabfbdfd3525520db4fd9aeb8ff91eaecde06e3

---
+++ config/bootanimation.mk
@@ -0,0 +1,14 @@
+# Boot Animation
+
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+    PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+    PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
