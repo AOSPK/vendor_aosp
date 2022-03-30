71efed5cc63191ee01a4ea5f79d41bfdebe85681
Alexandre Rangel <mamutal91@gmail.com>
2022-03-29 22:28:18 -0300

    vendor: Rebrand Kraken version
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,39 +1,39 @@
-# Copyright (C) 2016 The Pure Nexus Project
-# Copyright (C) 2016 The JDCTeam
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Kraken version
+KRAKEN_MOD_VERSION = 12.1
+KRAKEN_MOD_VERSION_PROP = twelve
+KRAKEN_BUILD_TYPE ?= UNOFFICIAL
+KRAKEN_BUILD_DATE = $(shell date +"%Y%m%d-%H%M")
+KRAKEN_BETA := false
+KRAKEN_VANILLA ?= false
+TARGET_USE_LAWNCHAIR ?= false
 
-KRAKEN_MOD_VERSION = v12.1
-KRAKEN_BUILD_TYPE := UNOFFICIAL
-KRAKEN_BUILD_ZIP_TYPE := VANILLA
-
-ifeq ($(KRAKEN_BETA),true)
-    KRAKEN_BUILD_TYPE := BETA
-endif
-
-ifeq ($(KRAKEN_GAPPS), true)
+# Build type
+ifeq ($(KRAKEN_VANILLA), true)
+    KRAKEN_GAPPS := false
+    KRAKEN_BUILD_ZIP_TYPE := Vanilla
+else
+    KRAKEN_GAPPS := true
+    KRAKEN_BUILD_ZIP_TYPE := GApps
     $(call inherit-product, vendor/gapps/common/common-vendor.mk)
-    KRAKEN_BUILD_ZIP_TYPE := GAPPS
 endif
 
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
-KRAKEN_VERSION := Kraken-$(KRAKEN_MOD_VERSION)-$(CURRENT_DEVICE)-$(KRAKEN_BUILD_TYPE)-$(shell date -u +%Y%m%d)-$(KRAKEN_BUILD_ZIP_TYPE)
+# Version
+ifeq ($(KRAKEN_BETA), true)
+    KRAKEN_VERSION := Kraken-$(KRAKEN_MOD_VERSION)-$(KRAKEN_BUILD_ZIP_TYPE)-$(KRAKEN_BUILD_DATE)-$(CURRENT_DEVICE)-BETA-UNOFFICIAL
+else ifeq ($(KRAKEN_BUILD_TYPE), OFFICIAL)
+    KRAKEN_VERSION := Kraken-$(KRAKEN_MOD_VERSION)-$(KRAKEN_BUILD_ZIP_TYPE)-$(KRAKEN_BUILD_DATE)-$(CURRENT_DEVICE)
+else
+    KRAKEN_VERSION := Kraken-$(KRAKEN_MOD_VERSION)-$(KRAKEN_BUILD_ZIP_TYPE)-$(KRAKEN_BUILD_DATE)-$(CURRENT_DEVICE)-UNOFFICIAL
+endif
 
+# Props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.kraken.version=$(KRAKEN_VERSION) \
   ro.kraken.releasetype=$(KRAKEN_BUILD_TYPE) \
-  ro.kraken.ziptype=$(KRAKEN_BUILD_ZIP_TYPE) \
+  ro.kraken.device=$(CURRENT_DEVICE) \
+  ro.kraken.ziptype=$(shell echo $(KRAKEN_BUILD_ZIP_TYPE) | tr A-Z a-z) \
   ro.modversion=$(KRAKEN_MOD_VERSION)
 
 KRAKEN_DISPLAY_VERSION := Kraken-$(KRAKEN_MOD_VERSION)-$(KRAKEN_BUILD_TYPE)
