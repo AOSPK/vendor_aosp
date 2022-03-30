a256a5e4dddf051303085017c1df85831f4ef7cc
Alexandre Rangel <mamutal91@gmail.com>
2022-03-29 22:25:10 -0300

    vendor: Enable 2-button navigation
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# NavigationBarMode
+PRODUCT_PACKAGES += \
+    NavigationBarMode2ButtonOverlay
+
 # Bootanimation
 include vendor/aosp/config/bootanimation.mk
 
